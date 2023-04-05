f3e693d301f9a0a990e0d0c6a3ba99a2398be9af
eddiewillers <derrick@pow.co>
2023-04-05 16:50:47 +0000

    Test1

--- src/App.tsx
+++ src/App.tsx
@@ -1,23 +1,30 @@
 import React from 'react';
 import logo from './logo.svg';
 import './App.css';
+import AnypaySDK from 'web3-checkout';
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        
+    
+        <button onClick={() => {
+          AnypaySDK({
+            element: 'anypay-widget',
+            config: {
+              invoiceId: 'GWT_QcGq-',
+              onAnypayLoadSuccess: console.log,
+              onAnypayLoadFailure: console.error,
+              onAnypayPaymentSuccess: console.log,
+              onAnypayPaymentFailure: console.error,
+            },
+          })
+        }}>
+            Click to Pay
+        </button>
+
       </header>
     </div>
   );
